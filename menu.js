636234ffdc1ff9bae6648e94845b0c6472558744
Tracy-miranja <barazatracy44@gmail.com>
2023-01-31 10:56:47 +0300

    add

---
+++ menu.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector("#hamburger");
+const navUl = document.querySelector("#links");
+
+hamburger.addEventListener("click", () => {
+  navUl.classList.toggle("show");
+});
