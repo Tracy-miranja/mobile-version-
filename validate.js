e676b712faf0b0fb0dcf2442be57269d8d22f16e
Tracy-miranja <barazatracy44@gmail.com>
2023-02-03 16:58:52 +0300

    add lowercase validation to my form

---
+++ validate.js
@@ -0,0 +1,27 @@
+function isLowerCase(inputText) {
+  return inputText.toLowerCase() === inputText;
+}
+
+const form = document.querySelector("form");
+const response = document.querySelector("#response");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const input = document.querySelector("input[type='email']");
+  const inputValue = input.value;
+
+  if (isLowerCase(inputValue)) {
+    response.classList.remove("d-none");
+    response.classList.add("d-flex");
+    response.classList.remove("danger");
+    response.classList.add("success");
+    response.textContent = "Submitted succesfully";
+  } else {
+    response.classList.remove("d-none");
+    response.classList.add("d-flex");
+    response.classList.remove("success");
+    response.classList.add("danger");
+    response.textContent = "Error! Email has to be in smallcase.";
+  }
+});
