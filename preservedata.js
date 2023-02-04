b469b9f5156c9c99210244d57ed414ef0bfdd1f9
Tracy-miranja <barazatracy44@gmail.com>
2023-02-04 03:08:35 +0300

    add to display content in the body if it persists

--- preservedata.js
+++ preservedata.js
@@ -6,19 +6,37 @@ function retrieveData() {
   return JSON.parse(localStorage.getItem('storedData')) || {};
 }
 
+// Select the inputs and textarea
 // eslint-disable-next-line no-undef
 const inputs = form.querySelectorAll('input');
+// eslint-disable-next-line no-undef
+const textarea = form.querySelector('textarea');
 
+// Retrieve the stored data
 const storedData = retrieveData();
+
+// Set the values of the inputs and textarea to the stored data
 inputs.forEach((input) => {
   if (storedData[input.name]) {
     input.value = storedData[input.name];
   }
 });
 
+if (storedData.body) {
+  textarea.value = storedData.body;
+}
+
+// Listen for changes in the form
 // eslint-disable-next-line no-undef
 form.addEventListener('input', (event) => {
   const input = event.target;
-  storedData[input.name] = input.value;
+  if (input.tagName === 'TEXTAREA') {
+    // Update the stored data with the new value
+    storedData.body = input.value;
+  } else {
+    // Update the stored data with the new value
+    storedData[input.name] = input.value;
+  }
+  // Store the updated data
   storeData(storedData);
 });
