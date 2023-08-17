72c6ec3a6696f0917afcdc6808fa898af480fd7a
Tracy-miranja <barazatracy44@gmail.com>
2023-08-17 13:33:46 +0300

    Modify about page

--- menu.js
+++ menu.js
@@ -13,3 +13,22 @@ navUl.addEventListener('click', () => {
   navUl.classList.remove('active');
   backGround.classList.remove('blur');
 });
+
+const toggleList = (event) => {
+  event.preventDefault();
+  const listItem = event.target.closest('li');
+  if (listItem) {
+    const arrow = listItem.querySelector('.arrow');
+    if (arrow) {
+      arrow.classList.toggle('down');
+      const list = listItem.querySelector('.list');
+      if (list) {
+        list.classList.toggle('active');
+      }
+    }
+  }
+};
+
+document.querySelectorAll('.frame, .skl').forEach((link) => {
+  link.addEventListener('click', toggleList);
+});
